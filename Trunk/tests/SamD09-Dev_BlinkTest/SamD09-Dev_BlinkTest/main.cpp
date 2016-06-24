9f9ce3bed9809d614bf05a836341e26f76e892f2
WeisTek <Jeremy.goss@weistekengineering.com>
2016-06-23 23:58:51 -0700

    smaller functions.

--- Trunk/tests/SamD09-Dev_BlinkTest/SamD09-Dev_BlinkTest/main.cpp
+++ Trunk/tests/SamD09-Dev_BlinkTest/SamD09-Dev_BlinkTest/main.cpp
@@ -12,11 +12,15 @@
 //void setPin(uint16_t pin);
 //void pinState(uint16_t pin);
 
-void init_TC1(void);
-
-void SetPinState(uint16_t op, uint16_t togg, uint16_t pin);
+//SamD09 specific registers i.e. only one PORT.
+#define setPin(pin) (REG_PORT_OUT0) |= (1 << (pin))
+#define clearpin(pin) (REG_PORT_OUT0) &= ~(1 << (pin))
+#define setDir(pin) (REG_PORT_DIR0) &= ~(1 << (pin))
 
 void wait(uint32_t	count);
+void init_TC1(void);
+
+//void SetPinState(uint16_t op, uint16_t togg, uint16_t pin);
 
 void init_TC1(void)
 {
@@ -40,7 +44,7 @@ void wait(uint32_t count)
 	
 	for(uint32_t x = 0;x>=count;x++);;
 }
-
+/*
 void SetPinState(uint16_t dir, uint16_t togg, uint16_t pin)
 {
 	//////////////////////////////////////////////////////////////////////////
@@ -63,36 +67,36 @@ void SetPinState(uint16_t dir, uint16_t togg, uint16_t pin)
 		REG_PORT_OUT0 &= ~(1 << pin);
 	}
 }
-
+*/
 void TC1_Handler()
 {
-	REG_PORT_OUTTGL0 = PORT_PA11;			// internal command to toggle pin.
+	//REG_PORT_OUTTGL0 = PORT_PA11;			// internal command to toggle pin.
 	REG_TC1_INTFLAG = TC_INTFLAG_OVF;		// reset interrupt flag
 }
-
 
 int main(void)
 {
     /* Initialize the SAM system */
     SystemInit();
-	init_TC1(); // <-- toggle the LED using TC1 interrupt handler.
+	//init_TC1(); // <-- toggle the LED using TC1 interrupt handler.
+	setDir(11);
 	
 	//REG_PORT_DIR0 |= (1 << 11); // Set the output of PB11 as output.
-	SetPinState(1,0,11); //set the output of PB11 as output using our own function.
+	//SetPinState(1,0,11); //set the output of PB11 as output using our own function.
 
     /* Replace with your application code */
     while (1) 
     {
 		
 		//REG_PORT_OUT0 &= ~(1 << 11); // Cycle pin high or low.
-		SetPinState(0,1,11); // Cycle pin high or low.
-		
-		 wait(100000);//Wait some time.
+		//SetPinState(0,1,11); // Cycle pin high or low.
+		setPin(11);
+		wait(100000);//Wait some time.
 		
 		//We can do this easier with this code segment.
 		//REG_PORT_DIR0 |= PORT_PA11; // Set the output of PB11 as output using its port definition.
-		SetPinState(0,1,11); // Cycle pin high or low.
-		
+		//SetPinState(0,1,11); // Cycle pin high or low.
+		clearpin(11);
 		wait(100000); //Wait some time.
 		
     }
