470cec24680d9848d5e0a284a863f4cbc45b02e7
WeisTekEng <Jeremy.goss@weistekengineering.com>
2016-07-04 09:10:37 -0700

    updated header with license info.

--- Trunk/Bootloader/MiniSamBootLoader/UART_Bootloader/main.c
+++ Trunk/Bootloader/MiniSamBootLoader/UART_Bootloader/main.c
@@ -8,6 +8,10 @@
  *					If PA15 bootpin is held low, micro will enter USART bootloader mode.
  *					if PA15 is high, micro runs user program if there is one at new start
  *					memory. Look at APP_START for start location of user flash.
+ *License:	                GNU GENERAL PUBLIC LICENSE Version 3 or newer. The header of this file may not change.
+ *				unless new features are added then the update section may be updated. The License file in 
+ *				the root of this repo Trunk/LICENSE should have been provided to you. If it was not you may 
+ *				find a copy of the GNU Open source license at https://www.gnu.org/licenses/gpl.html
  *
  *Modified:			07/04/2016
  *
